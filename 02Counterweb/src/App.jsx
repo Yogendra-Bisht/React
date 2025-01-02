6e5cb4a258842d435259ab4c39de846074ee0f99
Yogendra Bisht <bishtyogendra96436372@gmail.com>
2025-01-02 12:38:16 +0530

    created a simple counter using hooks concept of react

---
+++ 02Counterweb/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // let count =0;
+  const [count, setcount]=useState(0);
+  
+  let addone=()=>{
+    
+    setcount(count+1);
+  }
+  let removeone=()=>{
+    
+    setcount(count-1);
+  }
+  
+
+  return (
+    <>
+      <h3>Limited counts avalible</h3>
+      <br />
+      <button 
+      onClick={addone}>Add by one : {count}</button>
+      <br />
+      <br />
+      <button
+      onClick={removeone}
+      >Remove by one : {count}</button>
+
+      
+    </>
+  )
+}
+
+export default App
